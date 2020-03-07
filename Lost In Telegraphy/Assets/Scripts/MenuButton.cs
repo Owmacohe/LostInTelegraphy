ffcb6322a71d1470d137435bf2ab621d75b4b482
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2020-03-07 11:28:35 -0500

    Main menu added

---
+++ Lost In Telegraphy/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public string buttonType;
+    public string sceneName;
+
+    private void OnMouseDown()
+    {
+        if (buttonType == "start")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else if (buttonType == "exit")
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Lost In Telegraphy/Assets/Scripts/ScreenShift.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShift : MonoBehaviour
+{
+    public GameObject cam;
+    public string direction;
+    public float speed;
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(slider());
+    }
+
+    IEnumerator slider()
+    {
+        if (direction == "right")
+        {
+            int i;
+            for (i = 0; i < 16; i++)
+            {
+                cam.transform.Translate(1, 0, 0);
+                yield return new WaitForSeconds(speed);
+            }
+        }
+        else if (direction == "left")
+        {
+            int j;
+            for (j = 0; j < 16; j++)
+            {
+                cam.transform.Translate(-1, 0, 0);
+                yield return new WaitForSeconds(speed);
+            }
+        }
+    }
+}
